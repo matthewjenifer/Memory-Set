f17e7ea8f8f493effe726933ef7c2236cebbe11d
MATTHEW JENIFER <matthew36.j@gmail.com>
2021-12-31 04:40:49 -0500

    tenth commit: started work on functions. code stable..

--- js/script.js
+++ js/script.js
@@ -47,8 +47,23 @@ startTimer = () => {
 
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard, thirdCard
+
 function flipCard(){
-    this.classList.toggle('flip')
+    this.classList.add('flip')
+
+    if (!hasFlippedCard){
+        //first click
+        hasFlippedCard =  true;
+        firstCard = this;
+    }else{
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+    }
+    console.log(firstCard, secondCard)
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
