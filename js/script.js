5ff2015449b7f3a7c7911d3904b632ed564b6605
MATTHEW JENIFER <matthew36.j@gmail.com>
2021-12-31 03:30:21 -0500

    ninth commit: cards can flip now. second row delcared for possible set counter

--- js/script.js
+++ js/script.js
@@ -44,3 +44,11 @@ startTimer = () => {
         }
     }, 1000);
 };
+
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard(){
+    this.classList.toggle('flip')
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard))
