5f179426ef202085c1d9cae1665972b52958a466
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-01-03 00:00:28 -0500

    twentieth commit: minor adjustments made..struggling with connecting timer and score...

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
+//========================================
 const username = document.getElementById('username');
 const saveScorebtn = document.getElementById('saveScore');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores =JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 console.log(highScores);
 
-username.addEventListener('keyup', ()=>{
-    console.log(username.value);
+username.addEventListener('keyup', () => {
     saveScore.disabled = !username.value;
 })
 
-saveHighScore = (e) =>{
+saveHighScore = e => {
     console.log('you clicked the save button');
     e.preventDefault();
 
@@ -26,51 +26,21 @@ saveHighScore = (e) =>{
 
 //=================================================
 
-const timer = document.getElementById("timer");
-let timerInterval;
+var counter = 60;
 
-startTimer = () => {
-    // Firs twe start by clearing the existing timer, in case of a restart
-    clearInterval(timerInterval);
-    // Then we clear the variables
-    let second = 0,
-        minute = 0,
-        hour = 0,
-        odd = false;
-
-    // Next we set a interval every 1000 ms
-    timerInterval = setInterval(function () {
-        // check if we are odd or even and append class to timer
-        odd = !odd;
-        if (odd) {
-            timer.classList.add("odd");
+startTimer = () =>{
+    var interval = setInterval(function () {
+        counter--;
+        if (counter <= 0) {
+            clearInterval(interval);
+            $('#timer').html("<h3>Times Up!</h3>");
+            return;
         } else {
-            timer.classList.remove("odd");
-        }
-
-        // We set the timer text to include a two digit representation
-        timer.innerHTML =
-            (hour ? hour + ":" : "") +
-            (minute < 10 ? "0" + minute : minute) +
-            ":" +
-            (second < 10 ? "0" + second : second);
-
-        // Next we add a new second since one second is passed
-        second++;
-
-        // We check if the second equals 60 "one minute"
-        if (second == 60) {
-            // If so, we add a minute and reset our seconds to 0
-            minute++;
-            second = 0;
-        }
-
-        // If we hit 60 minutes "one hour" we reset the minutes and plus an hour
-        if (minute == 60) {
-            hour++;
-            minute = 0;
+            $('#time').text(counter);
+            console.log("Timer --> " + counter);
         }
     }, 1000);
+
 };
 
 //=========================================
@@ -96,6 +66,7 @@ function flipCard() {
     this.classList.add('flip')
 
     if (!hasFlippedCard) {
+        startTimer();
         //first click
         hasFlippedCard = true;
         firstCard = this;
@@ -103,6 +74,7 @@ function flipCard() {
             hasFlippedCard,
             firstCard
         })
+        
         console.log(this.dataset) // set group identified
 
     } else if (hasFlippedCard === true && hasFlippedSecondCard !== true) {
@@ -189,12 +161,3 @@ function resetBoard() {
 
 
 //============================
-
-// const ruleSpread = document.querySelectorAll('.ruleIMG')
-// const IMG = document.querySelector('ruleSheets')
-
-// const toggleFullScreen = () => {
-//     IMG.requestFullscreen()
-// }
-
-// ruleSpread.addEventListener('click', toggleFullScreen)
